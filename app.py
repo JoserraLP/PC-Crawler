df64b20607b2dbda4904bb38bb19ee642825c45c
JoserraLP <jolozanop@alumnos.unex.es>
2020-02-28 19:03:42 +0100

    feat: Add folder crawling and extension filter

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
 from os import listdir, path
 import sys
 
-def getCurrentFiles(directory):
-    files = [f for f in listdir(directory)]
-    print(files)
+wordsMap = dict()
+
+def getCurrentFiles(directory,ext_ignore=()):
+    for file in listdir(directory):
+        dirFile=directory+'\\'+file
+        print(dirFile)
+        if path.isdir(dirFile):
+            getCurrentFiles(dirFile,ext_ignore=ext_ignore)
+        else:
+            if not dirFile.endswith(ext_ignore):
+                processFile(dirFile)
+
+def processFile(filename):
+    with open(filename) as file:
+        words = file.read().split()
+        for word in words:
+            updateMap(word)
+
+def updateMap(word):
+    if wordsMap.get(word) is None:
+        wordsMap[word]=1
+    else:
+        wordsMap[word]+=1
 
 if __name__ == "__main__":
-    getCurrentFiles(sys.argv[1])
+    with open("file_ext_ignore.txt", "r") as file_ext_ignore:
+        exts=tuple(file_ext_ignore.read().split())
+
+    getCurrentFiles(sys.argv[1],exts)
+    
+    with open(sys.argv[2], "w+") as file:
+        for key, value in sorted(wordsMap.items()):
+            file.write(f'{key} : {value} \n')

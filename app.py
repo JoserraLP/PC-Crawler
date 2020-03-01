b4ab26277c39ee058b982849e489fa50acf53e97
JoserraLP <jolozanop@alumnos.unex.es>
2020-03-01 17:21:26 +0100

    refactor: change function name

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import sys
 
 wordsMap = dict()
 
-def getCurrentFiles(directory,ext_ignore=()):
+def getWordFrequencyOnDirectory(directory,ext_ignore=()):
     for file in listdir(directory):
         dirFile=directory+'\\'+file
         print(dirFile)
         if path.isdir(dirFile):
-            getCurrentFiles(dirFile,ext_ignore=ext_ignore)
+            getWordFrequencyOnDirectory(dirFile,ext_ignore=ext_ignore)
         else:
             if not dirFile.endswith(ext_ignore):
                 processFile(dirFile)
@@ -29,7 +29,7 @@ if __name__ == "__main__":
     with open("file_ext_ignore.txt", "r") as file_ext_ignore:
         exts=tuple(file_ext_ignore.read().split())
 
-    getCurrentFiles(sys.argv[1],exts)
+    getWordFrequencyOnDirectory(sys.argv[1],exts)
     
     with open(sys.argv[2], "w+") as file:
         for key, value in sorted(wordsMap.items()):

5124d2c1eb6397bc292d67950b1f3d542c2a9d85
franusierra <franusierra@gmail.com>
2020-03-11 16:55:14 +0100

    refactor: replace wordmap global variable for parameter

--- app.py
+++ app.py
@@ -2,25 +2,23 @@ from os import listdir, path
 import sys
 import re
 
-wordsMap = dict()
-
-def getWordFrequencyOnDirectory(directory,file_ext_accept=()):
+def getWordFrequencyOnDirectory(directory,file_ext_accept=(),wordsMap=dict()):
     for file in listdir(directory):
         dirFile=directory+'\\'+file
         print(dirFile)
         if path.isdir(dirFile):
-            getWordFrequencyOnDirectory(dirFile,file_ext_accept=file_ext_accept)
+            getWordFrequencyOnDirectory(dirFile,file_ext_accept=file_ext_accept,wordsMap=wordsMap)
         else:
             if dirFile.endswith(file_ext_accept):
-                processFile(dirFile)
+                processFile(dirFile,wordsMap)
 
-def processFile(filename):
+def processFile(filename,wordsMap):
     with open(filename) as file:
         words = [word.lower() for word in re.split(r'\W+',file.read())]
         for word in words:
-            updateMap(word)
+            updateMap(word,wordsMap)
 
-def updateMap(word):
+def updateMap(word,wordsMap):
     if wordsMap.get(word) is None:
         wordsMap[word]=1
     else:
@@ -29,9 +27,9 @@ def updateMap(word):
 if __name__ == "__main__":
     with open("file_ext_accept.txt", "r") as file_ext_accept:
         exts=tuple(file_ext_accept.read().split())
-
-    getWordFrequencyOnDirectory(sys.argv[1],exts)
+    wordFrequency=dict()
+    getWordFrequencyOnDirectory(sys.argv[1],exts,wordsMap=wordFrequency)
 
     with open(sys.argv[2], "w+") as file:
-        for key, value in sorted(wordsMap.items()):
+        for key, value in sorted(wordFrequency.items()):
             file.write(f'{key} : {value} \n')

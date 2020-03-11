b66af40f33619e07cfda94f9ea2ed59c313758d8
JoserraLP <jolozanop@alumnos.unex.es>
2020-03-11 19:47:21 +0100

    feat: add cache and user interaction

--- app.py
+++ app.py
@@ -1,35 +1,15 @@
-from os import listdir, path
+import load_word_frequency as Loader
 import sys
-import re
-
-def getWordFrequencyOnDirectory(directory,file_ext_accept=(),wordsMap=dict()):
-    for file in listdir(directory):
-        dirFile=directory+'\\'+file
-        print(dirFile)
-        if path.isdir(dirFile):
-            getWordFrequencyOnDirectory(dirFile,file_ext_accept=file_ext_accept,wordsMap=wordsMap)
-        else:
-            if dirFile.endswith(file_ext_accept):
-                processFile(dirFile,wordsMap)
-
-def processFile(filename,wordsMap):
-    with open(filename) as file:
-        words = [word.lower() for word in re.split(r'\W+',file.read())]
-        for word in words:
-            updateMap(word,wordsMap)
-
-def updateMap(word,wordsMap):
-    if wordsMap.get(word) is None:
-        wordsMap[word]=1
-    else:
-        wordsMap[word]+=1
 
 if __name__ == "__main__":
     with open("file_ext_accept.txt", "r") as file_ext_accept:
         exts=tuple(file_ext_accept.read().split())
-    wordFrequency=dict()
-    getWordFrequencyOnDirectory(sys.argv[1],exts,wordsMap=wordFrequency)
+    
+    words_freq = Loader.loadFrequencies(sys.argv[1],exts=exts)
 
-    with open(sys.argv[2], "w+") as file:
-        for key, value in sorted(wordFrequency.items()):
-            file.write(f'{key} : {value} \n')
+    while True:
+        word=input("\nIntroduzca su busqueda: \n(Salir => /exit)\nWord: ")
+        if (word != '/exit'):
+            print(f'Freq: {words_freq.get(word)}')
+        else:
+            break
---
+++ load_word_frequency.py
@@ -0,0 +1,37 @@
+from os import listdir, path
+import sys, re, json
+
+def getWordFrequencyOnDirectory(directory,file_ext_accept=(),wordsMap=dict()):
+    for file in listdir(directory):
+        dirFile=directory+'\\'+file
+        if path.isdir(dirFile):
+            getWordFrequencyOnDirectory(dirFile,file_ext_accept=file_ext_accept,wordsMap=wordsMap)
+        else:
+            if dirFile.endswith(file_ext_accept):
+                processFile(dirFile,wordsMap)
+
+def processFile(filename, wordsMap):
+    with open(filename) as file:
+        words = [word.lower() for word in re.split(r'\W+',file.read())]
+        for word in words:
+            updateMap(word,wordsMap)
+
+def updateMap(word, wordsMap):
+    if wordsMap.get(word) is None:
+        wordsMap[word]=1
+    else:
+        wordsMap[word]+=1
+
+def loadFrequencies(dir_name,exts=tuple()):
+    try:
+        dict_file=open(f'.cache/{dir_name}.cache', 'r')
+        wordFrequency=json.load(dict_file)
+        print(f'Loaded file .cache/{dir_name}.cache')
+    except:
+        wordFrequency=dict()
+        getWordFrequencyOnDirectory(dir_name,exts,wordsMap=wordFrequency)
+
+    with open(f'.cache/{dir_name}.cache', "w+") as file:
+        json.dump(dict(sorted(wordFrequency.items())),file)
+    
+    return wordFrequency

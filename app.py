a914dffe0b990e8a79567236a6329d78495003c5
JoserraLP <jolozanop@alumnos.unex.es>
2020-03-11 16:35:57 +0100

    feat: change extension file order, count only words no case sensitive

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 from os import listdir, path
 import sys
+import re
 
 wordsMap = dict()
 
-def getWordFrequencyOnDirectory(directory,ext_ignore=()):
+def getWordFrequencyOnDirectory(directory,file_ext_accept=()):
     for file in listdir(directory):
         dirFile=directory+'\\'+file
         print(dirFile)
         if path.isdir(dirFile):
-            getWordFrequencyOnDirectory(dirFile,ext_ignore=ext_ignore)
+            getWordFrequencyOnDirectory(dirFile,file_ext_accept=file_ext_accept)
         else:
-            if not dirFile.endswith(ext_ignore):
+            if dirFile.endswith(file_ext_accept):
                 processFile(dirFile)
 
 def processFile(filename):
     with open(filename) as file:
-        words = file.read().split()
+        words = [word.lower() for word in re.split(r'\W+',file.read())]
         for word in words:
             updateMap(word)
 
@@ -26,11 +27,11 @@ def updateMap(word):
         wordsMap[word]+=1
 
 if __name__ == "__main__":
-    with open("file_ext_ignore.txt", "r") as file_ext_ignore:
-        exts=tuple(file_ext_ignore.read().split())
+    with open("file_ext_accept.txt", "r") as file_ext_accept:
+        exts=tuple(file_ext_accept.read().split())
 
     getWordFrequencyOnDirectory(sys.argv[1],exts)
-    
+
     with open(sys.argv[2], "w+") as file:
         for key, value in sorted(wordsMap.items()):
             file.write(f'{key} : {value} \n')

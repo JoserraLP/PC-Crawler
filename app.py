e942bda6dc5c36ac5bc96d1aa280c7925269a404
franusierra <franusierra@gmail.com>
2020-03-15 20:14:11 +0100

    feat: add thesaurus system

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 import load_word_frequency as Loader
+import thesaurus as Thesaurus
 import sys
 
 if __name__ == "__main__":
     with open("file_ext_accept.txt", "r") as file_ext_accept:
         exts=tuple(file_ext_accept.read().split())
+    thes=Thesaurus.createThesaurus('Thesaurus_es_ES.txt')
+    words_freq = Loader.loadFrequencies(sys.argv[1],exts=exts,acceptedWords=thes.keys())
     
-    words_freq = Loader.loadFrequencies(sys.argv[1],exts=exts)
-
     while True:
-        word=input("\nIntroduzca su busqueda: \n(Salir => /exit)\nWord: ")
+        word=input("\nIntroduzca su busqueda de sinónimos: \n(Salir => /exit)\nBusqueda: ")
         if (word != '/exit'):
-            freq = words_freq.get(word)
-            print(f'Freq: {freq}') if freq is not None else print(f'Freq: 0')
+            sinonims=thes.get(word)
+            total_freq=0
+            if(sinonims!=None):
+                for sinonim in sinonims:
+                    freq=words_freq.get(sinonim)
+                    print(f'-{sinonim}: {freq if not freq is None else 0}')
+                    if not freq is None:
+                        total_freq+=freq
+                print(f'Frequencia total: {total_freq}')
         else:
             break
--- load_word_frequency.py
+++ load_word_frequency.py
@@ -1,36 +1,37 @@
 from os import listdir, path
 import sys, re, json, unidecode
 
-def getWordFrequencyOnDirectory(directory,file_ext_accept=(),wordsMap=dict()):
+def getWordFrequencyOnDirectory(directory,file_ext_accept=(),wordsMap=dict(),acceptedWords=None):
     for file in listdir(directory):
         dirFile=directory+'\\'+file
         if path.isdir(dirFile):
-            getWordFrequencyOnDirectory(dirFile,file_ext_accept=file_ext_accept,wordsMap=wordsMap)
+            getWordFrequencyOnDirectory(dirFile,file_ext_accept=file_ext_accept,wordsMap=wordsMap,acceptedWords=acceptedWords)
         else:
             print(dirFile)
             if dirFile.endswith(file_ext_accept):
-                processFile(dirFile,wordsMap)
+                processFile(dirFile,wordsMap,acceptedWords)
 
-def processFile(filename, wordsMap):
+def processFile(filename, wordsMap,acceptedWords=None):
     with open(filename, encoding='utf-8') as file:
         words = [word.lower() for word in re.split(r'\W+',unidecode.unidecode(file.read()))]
         for word in words:
-            updateMap(word,wordsMap)
+            updateMap(word,wordsMap,acceptedWords=acceptedWords)
 
-def updateMap(word, wordsMap):
-    if wordsMap.get(word) is None:
-        wordsMap[word]=1
-    else:
-        wordsMap[word]+=1
+def updateMap(word, wordsMap,acceptedWords=None):
+    if (acceptedWords is None) or word in acceptedWords:
+        if wordsMap.get(word) is None:
+            wordsMap[word]=1
+        else:
+            wordsMap[word]+=1
 
-def loadFrequencies(dir_name,exts=tuple()):
+def loadFrequencies(dir_name,exts=tuple(),acceptedWords=None):
     try:
         dict_file=open(f'.cache/{dir_name}.cache', 'r')
         wordFrequency=json.load(dict_file)
         print(f'Loaded file .cache/{dir_name}.cache')
     except:
         wordFrequency=dict()
-        getWordFrequencyOnDirectory(dir_name,exts,wordsMap=wordFrequency)
+        getWordFrequencyOnDirectory(dir_name,exts,wordsMap=wordFrequency,acceptedWords=acceptedWords)
 
     with open(f'.cache/{dir_name}.cache', "w+") as file:
         json.dump(dict(sorted(wordFrequency.items())),file)
